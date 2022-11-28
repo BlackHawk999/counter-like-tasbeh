d44e86debef27cf57fcbb203985a9a96a7f003a8
elyor010 <elsadulaev@gmail.com>
2022-11-29 00:09:30 +0500

    first commit and  hope a lods of commit in this rep

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,98 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="count">
+    <div class="counts-wrapper">
+      <div class="counter-child">
+        <p class="count-title">Current amount</p>
+        <h6 class="amount-balls">{{ count }}</h6>
+      </div>
+      <button @click="increment" class="increment">+</button>
+      <div class="counter-child">
+        <p class="count-title">Counter of all amount</p>
+        <h6 class="amount-balls all">{{ allCount }}</h6>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+const MAX_AMOUNT = 33;
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  data() {
+    return {
+      amount: 0,
+    };
+  },
+  computed: {
+    allCount() {
+      if (!this.amount) return 0;
+
+      return Math.floor(this.amount / MAX_AMOUNT);
+    },
+    count() {
+      if (!this.allCount) return this.amount;
+
+      return this.amount - this.allCount * MAX_AMOUNT;
+    },
+  },
+  methods: {
+    increment() {
+      this.amount++;
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.count {
+  .counts-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .increment {
+    margin-top: 15px;
+    margin-bottom: 20px;
+    font-size: 25px;
+    color: #fff;
+    background-color: rgba(159, 66, 199, 0.85);
+    border: 2px solid #bd3fef;
+    outline: none;
+    transition: 0.3s all;
+    cursor: pointer;
+
+    &:hover {
+      background-color: #fff;
+      color: #bd3fef;
+    }
+  }
+
+  &-child {
+    display: flex;
+    flex-direction: column;
+  }
+  
+  &-title {
+    font-size: 28px;
+    font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+      "Lucida Sans", Arial, sans-serif;
+      font-weight: 600;
+      margin: 0;
+  }
+
+  .amount-balls {
+    font-size: 35px;
+    margin: 0;
+
+    &:not(:last-child) {
+      margin-right: 20px;
+    }
+
+    &.all {
+      font-size: 30px;
+    }
+  }
 }
 </style>
